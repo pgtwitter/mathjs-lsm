e42d0b3f74fe587ba6543f552d5cb5e1fe31222e
Work <work@locahost>
2018-04-28 14:41:40 +0900

    [modified] divided the L2 function.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const [g, ys, xs] = d(100, ans, fn)
 console.log(g, ys);
 
 Array.forEach([0.0, 0.01, 0.1, 1], function(lamda) {
-	const prediction = mathjsLsm.lsm(g, ys, lamda);
+	const prediction = lamda == 0 ? mathjsLsm.lsm(g, ys) : mathjsLsm.lsmL2(g, ys, lamda);
 	const r = mathjsLsm.residual(prediction, fn, xs, ys);
 	console.log('lamda', lamda, 'prediction', prediction, 'residual', r);
 });
--- mathjsLsm.js
+++ mathjsLsm.js
@@ -1,21 +1,29 @@
 (function() {
 	window.mathjsLsm = {
-		lsm: function(g, y, lamda = 0.0) {
+		lsm: function(g, y) {
 			const qr = math.qr(g);
 			const Qt = math.transpose(qr.Q);
 			const Rt = math.transpose(qr.R);
-			let RtR = math.multiply(Rt, qr.R);
-			if (lamda != 0.0) {
-				const RtR_size = math.size(RtR)
-				const I = math.eye(RtR_size[0])._data;
-				const LamdaI = math.multiply(lamda, I);
-				const RtRaddLamda = math.add(RtR, LamdaI);
-				RtR = RtRaddLamda;
-			}
+			const RtR = math.multiply(Rt, qr.R);
 			const RtR_i = math.inv(RtR);
 			const RtR_iRt = math.multiply(RtR_i, Rt);
 			const RtR_iRtQt = math.multiply(RtR_iRt, Qt);
 			const a = math.multiply(RtR_iRtQt, y);
+			return math.flatten(a);
+		},
+		lsmL2: function(g, y, lamda = 0.0) {
+			const qr = math.qr(g);
+			const Qt = math.transpose(qr.Q);
+			const Rt = math.transpose(qr.R);
+			const RtR = math.multiply(Rt, qr.R);
+			const RtR_size = math.size(RtR)
+			const I = math.eye(RtR_size[0])._data;
+			const LamdaI = math.multiply(lamda, I);
+			const RtRaddL = math.add(RtR, LamdaI);
+			const RtRaddL_i = math.inv(RtRaddL);
+			const RtRaddL_iRt = math.multiply(RtRaddL_i, Rt);
+			const RtRaddL_iRtQt = math.multiply(RtRaddL_iRt, Qt);
+			const a = math.multiply(RtRaddL_iRtQt, y);
 			return math.flatten(a);
 		},
 		residual: function(coeff, fn, x, y) {
